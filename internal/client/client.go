2c9dfc63fbe738e6e71d08945fbe22adf78d0f00
Francisco Montoya <ext_jfranmontoya@falabella.cl>
2022-03-06 23:29:00 -0300

    work in progress

--- internal/client/client.go
+++ internal/client/client.go
@@ -1,12 +1,56 @@
 package client
 
-import "context"
+import (
+	"context"
+	"log"
+	"ltgc-api-create-user/internal/entity"
+	"time"
 
-type Firestore interface {
-	Execute(ctx context.Context)
+	"cloud.google.com/go/firestore"
+	"google.golang.org/api/option"
+)
+
+type Repository interface {
+	CreateUser(ctx context.Context, user *entity.User) (*entity.User, error)
 }
 
-type client struct {
-	firestore Firestore
+type firestoreRepository struct {
+	collection string
+	client     *firestore.Client
+	timeout    time.Duration
 }
 
+func newClient(file string, projectID string, timeout int) (*firestore.Client, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(timeout)*time.Second)
+	defer cancel()
+	sa := option.WithCredentialsFile(file)
+	client, err := firestore.NewClient(ctx, projectID, sa)
+	if err != nil {
+		return nil, err
+	}
+	return client, nil
+}
+
+func NewFirestorerepository(fileName string, projectID string, collectionName string) Repository {
+	client, err := newClient(fileName, projectID, 10)
+	if err != nil {
+		log.Println("error creando un cliente firestore %w", err)
+	}
+	r := &firestoreRepository{
+		collection: collectionName,
+		client:     client,
+		timeout:    time.Duration(10) * time.Second,
+	}
+
+	return r
+}
+
+func (r *firestoreRepository) CreateUser(ctx context.Context, user *entity.User) (*entity.User, error) {
+
+	_, err := r.client.Collection(r.collection).Doc(user.Name).Set(ctx, user)
+	if err != nil {
+		return &entity.User{}, err
+	}
+
+	return user, nil
+}
